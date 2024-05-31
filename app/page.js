8769c0176ff724ae35c91437cc1eb5f2f78c8099
Abdul Barik <abdulbarik1997m@gmail.com>
2024-06-01 01:12:37 +0600

    updates

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Navbar />
       <Banner />
-    </div>
+      <Footer />
+    </>
   );
 }
